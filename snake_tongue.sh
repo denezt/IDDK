989fe123ce0e7d00020d69154cf1db2fe0e75aa0
Richard Jackson <denezt@yahoo.com>
2015-07-05 21:29:22 -0500

    Uploading Scripts

--- snake_tongue.sh
+++ snake_tongue.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#NAME: 		Snake Tongue
+#PURPOSE:	Provides the password
+#		detection list.
+#Created By:	Richard L. Jackson
+#
+#USAGE:		./snake_tongue.sh [ITERATION]
 
 limit=$1
 venom="striker"
@@ -12,12 +18,12 @@ then
 		do
 			alpha=( 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z' )
 			symbol=( '!' '@' '#' '$' '%' '^' '&' '*' '(' ')' '-' '_' '+' )
-			#uuidgen | tr "-" "\n" | tee -a $venom$head
-			#printf "Random[0]\t|%i|%i\n" $(echo $RANDOM | cut -c -2) $(echo $RANDOM | cut -c 2-)
-			printf "%c%i%i" ${alpha[$(echo $RANDOM | cut -c 1)]} $(echo $RANDOM | cut -c -2) $(echo $RANDOM | cut -c 2-)
+			uuidgen | tr "-" "\n" | tee -a $venom$head
+			printf "Random[0]\t|%i|%i\n" $(echo $RANDOM | cut -c -2) $(echo $RANDOM | cut -c 2-)
+			printf "%c%i%i\n" ${alpha[$(echo $RANDOM | cut -c 1)]} $(echo $RANDOM | cut -c -2) $(echo $RANDOM | cut -c -2)
 			for b in {0..10}
 			do
-				printf "Random[1]:\t%s%s%s\n" "$RANDOM${alpha[$a]}" "${symbol[$b]}" "$(echo $RANDOM | cut -c 2)"
+				printf "%s%s%s\n" "$RANDOM${alpha[$a]}" "${symbol[$b]}" "$(echo $RANDOM | cut -c 2)"
 			done
 		done
 	done
